c195379e48b57a52aeb2dc042f82e2ce1552e524
Y-creator249 <yanncedricemmanuelo@gmail.com>
2026-03-06 14:34:14 +0000

    fix: update CSRF_TRUSTED_ORIGINS for Render

--- portfolioAngularBackend/settings.py
+++ portfolioAngularBackend/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
 CSRF_TRUSTED_ORIGINS = config(
     'CSRF_TRUSTED_ORIGINS',
-    default='https://web-production-78b23.up.railway.app',
+    default='https://portfolio-backend-mqgp.onrender.com',
     cast=Csv()
 )
 
